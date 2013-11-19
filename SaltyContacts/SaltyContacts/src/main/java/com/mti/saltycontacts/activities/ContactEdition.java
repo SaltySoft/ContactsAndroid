4da6f2b643028ce1946ee7a53d2a6acde1180153
Antoine Jackson <a.j.william26@gmail.com>
2013-11-19 20:55:20 +0100

    + Added add contact button and edition activity

--- SaltyContacts/SaltyContacts/src/main/java/com/mti/saltycontacts/activities/ContactEdition.java
+++ SaltyContacts/SaltyContacts/src/main/java/com/mti/saltycontacts/activities/ContactEdition.java
@@ -3,7 +3,6 @@ package com.mti.saltycontacts.activities;
 import android.app.Activity;
 import android.app.ActionBar;
 import android.app.Fragment;
-import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -12,31 +11,31 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
-import android.widget.Button;
-
 import com.mti.saltycontacts.R;
 
-public class MainActivity extends Activity implements View.OnClickListener {
+import java.util.logging.Logger;
+
+public class ContactEdition extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
+        Log.d("ANTOINE", "started activity");
+        setContentView(R.layout.activity_contact_edition);
+        Log.d("ANTOINE", "created view");
         if (savedInstanceState == null) {
             getFragmentManager().beginTransaction()
                     .add(R.id.container, new PlaceholderFragment())
                     .commit();
         }
-
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-
+        
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        getMenuInflater().inflate(R.menu.contact_edition, menu);
         return true;
     }
 
@@ -45,26 +44,11 @@ public class MainActivity extends Activity implements View.OnClickListener {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        switch (item.getItemId()) {
-            case R.id.action_settings:
-                return true;
-            case R.id.add_contact_button:
-                Intent intent = new Intent(this, ContactEdition.class);
-                Log.d("ANTOINE", "trying to start");
-                startActivity(intent);
-                Log.d("ANTOINE", "tried to start");
-                return true;
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
         }
-
         return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-           default:
-               break;
-        }
     }
 
     /**
@@ -77,8 +61,8 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+                Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_contact_edition, container, false);
             return rootView;
         }
     }
