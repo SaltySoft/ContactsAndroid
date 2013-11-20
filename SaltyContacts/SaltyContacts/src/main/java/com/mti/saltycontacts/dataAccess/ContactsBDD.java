2f5124dea14b484f7067788588ff235f366cf9de
Antoine Jackson <a.j.william26@gmail.com>
2013-11-20 15:30:20 +0100

    + Added possibility to add contacts to db

--- SaltyContacts/SaltyContacts/src/main/java/com/mti/saltycontacts/dataAccess/ContactsBDD.java
+++ SaltyContacts/SaltyContacts/src/main/java/com/mti/saltycontacts/dataAccess/ContactsBDD.java
@@ -2,9 +2,12 @@ package com.mti.saltycontacts.dataAccess;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
 import com.mti.saltycontacts.models.Contact;
+
+import java.util.ArrayList;
 
 /**
  * Created by Antoine on 11/20/13.
@@ -16,9 +19,13 @@ public class ContactsBDD {
 
     private static final String TABLE_CONTACTS = "table_contacts";
     private static final String COL_CONTACT_ID = "ID";
+    private static final int NUM_CONTACT_COL_ID = 0;
     private static final String COL_CONTACT_FIRSTNAME = "FIRSTNAME";
+    private static final int NUM_CONTACT_COL_FIRSTNAME = 1;
     private static final String COL_CONTACT_LASTNAME = "LASTNAME";
+    private static final int NUM_CONTACT_COL_LASTNAME = 2;
     private static final String COL_CONTACT_ADDRESS = "ADDRESS";
+    private static final int NUM_CONTACT_COL_ADDRESS = 3;
 
     private static final String TABLE_EMAIL = "table_emails";
     private static final String COL_EMAIL_ID = "ID";
@@ -84,5 +91,49 @@ public class ContactsBDD {
         return bdd.delete(TABLE_CONTACTS, COL_CONTACT_ID + " = " + contact.getId(), null);
     }
 
+    public Contact getContact(long id) {
+        Cursor c = bdd.query(TABLE_CONTACTS, new String[]
+                {COL_CONTACT_ID, COL_CONTACT_FIRSTNAME, COL_CONTACT_LASTNAME, COL_CONTACT_ADDRESS},
+                COL_CONTACT_ID + " = " + id, null, null, null, COL_CONTACT_ID);
+        return cursorToContact(c);
+    }
+
+    public Contact cursorToContact(Cursor c) {
+        if (c.getCount() == 0) {
+            c.close();
+            return null;
+        }
+
+        Contact contact = new Contact();
+        contact.setId(c.getInt(NUM_CONTACT_COL_ID));
+        contact.setFirstName(c.getString(NUM_CONTACT_COL_FIRSTNAME));
+        contact.setLastName(c.getString(NUM_CONTACT_COL_LASTNAME));
+        contact.setPostalAddress(c.getString(NUM_CONTACT_COL_ADDRESS));
+        c.close();
+        return contact;
+    }
+
+    public ArrayList<Contact> getAllContacts() {
+        Cursor c = bdd.query(TABLE_CONTACTS, new String[]
+                {COL_CONTACT_ID, COL_CONTACT_FIRSTNAME, COL_CONTACT_LASTNAME, COL_CONTACT_ADDRESS},
+                null, null, null, null, COL_CONTACT_ID);
+
+        if (c.getCount() == 0) {
+            c.close();
+            return null;
+        }
+
+        ArrayList<Contact> contact_list = new ArrayList<Contact>();
+        while (c.moveToNext()) {
+            Contact contact = new Contact();
+            contact.setId(c.getInt(NUM_CONTACT_COL_ID));
+            contact.setFirstName(c.getString(NUM_CONTACT_COL_FIRSTNAME));
+            contact.setLastName(c.getString(NUM_CONTACT_COL_LASTNAME));
+            contact.setPostalAddress(c.getString(NUM_CONTACT_COL_ADDRESS));
+            contact_list.add(contact);
+        }
+        c.close();
+        return contact_list;
+    }
 
 }
