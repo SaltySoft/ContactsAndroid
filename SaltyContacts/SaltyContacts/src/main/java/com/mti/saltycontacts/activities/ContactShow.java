11f139d002065f90c87750f8d6a06d3a4cb3b262
Vincent Lefebvre <vinc.lefebv@gmail.com>
2013-11-22 17:39:11 +0100

    * merging.

--- SaltyContacts/SaltyContacts/src/main/java/com/mti/saltycontacts/activities/ContactShow.java
+++ SaltyContacts/SaltyContacts/src/main/java/com/mti/saltycontacts/activities/ContactShow.java
@@ -140,9 +140,8 @@ public class ContactShow extends Activity implements View.OnClickListener {
                     Log.i(LOG_TAG, "restart app");
                     // restart app
                     Intent i = getBaseContext().getPackageManager().getLaunchIntentForPackage(getBaseContext().getPackageName());
-                    i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+//                    i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                     startActivity(i);
-
                     isPhoneCalling = false;
                 }
             }
