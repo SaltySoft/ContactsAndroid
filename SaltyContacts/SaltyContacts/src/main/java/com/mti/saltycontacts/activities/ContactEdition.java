b7dc68d1c346c8d6a3595bcfcd366d78a45f9266
Vincent Lefebvre <vinc.lefebv@gmail.com>
2013-11-24 21:55:11 +0100

    * merge.

--- SaltyContacts/SaltyContacts/src/main/java/com/mti/saltycontacts/activities/ContactEdition.java
+++ SaltyContacts/SaltyContacts/src/main/java/com/mti/saltycontacts/activities/ContactEdition.java
@@ -187,6 +187,7 @@ public class ContactEdition extends Activity implements View.OnClickListener {
         switch (item.getItemId()) {
             case android.R.id.home:
                 finish();
+                break;
             case R.id.action_save:
                 saveContact();
                 break;
